d61ec0a8fb22d7ea10956a86b6c4d1a47a3facb7
Ankush Anand <ankushanand718@gmail.com>
2026-03-01 21:25:26 +0530

    Update binary_sensor.py

--- custom_components/unified_remote/binary_sensor.py
+++ custom_components/unified_remote/binary_sensor.py
@@ -2,18 +2,15 @@
 from homeassistant.components.binary_sensor import BinarySensorEntity, BinarySensorDeviceClass
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.device_registry import DeviceInfo
 
 from .const import DOMAIN
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
     """Set up the Unified Remote binary sensor."""
     computers = hass.data[DOMAIN].get("computers", [])
-    
-    entities = []
-    for computer in computers:
-        entities.append(UnifiedRemoteStatusSensor(computer))
+    async_add_entities([UnifiedRemoteStatusSensor(comp) for comp in computers], True)
 
-    async_add_entities(entities, True)
 
 class UnifiedRemoteStatusSensor(BinarySensorEntity):
     """Sensor to track if the Unified Remote server is connected."""
@@ -23,6 +20,16 @@ class UnifiedRemoteStatusSensor(BinarySensorEntity):
         self._attr_name = f"{computer.name} Connection"
         self._attr_unique_id = f"{computer.host}_connection_status"
         self._attr_device_class = BinarySensorDeviceClass.CONNECTIVITY
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        """Return device information to link this entity to the computer."""
+        return DeviceInfo(
+            identifiers={(DOMAIN, self._computer.host)},
+            name=self._computer.name,
+            manufacturer="Unified Remote",
+            model="Computer Status",
+        )
 
     @property
     def is_on(self):
